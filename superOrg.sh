2cf812029ab6b171d20d382e3c3b487903a436dc
vamshinath <vamshiapple.af@gmail.com>
2016-10-24 19:30:51 +0530

    PATH TESTING

--- superOrg.sh
+++ superOrg.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 clear
-echo "Started working "
+echo -e "\n\t\tHi $USER"
+if (( !$# ))
+then
+	read -p "Enter Directory to Organize:" dr
+	cd $dr
+	if (( $? ))
+	then 
+		read -p "Please Enter Correct Path:" dr
+		cd $dr
+		if (( $? ))
+		then
+			echo "Usage:./superOrg.sh  dir_name"
+			exit 0
+		fi
+	fi
+else
+	cd $1
+	if (( $? ))
+	then
+		echo -e "\n\t WRONG PATH"
+		echo "Usage:./superOrg.sh  dir_name"
+		sleep 0.2
+		exit 0
+	fi
+fi
+echo "SUCCESS"
+
 
