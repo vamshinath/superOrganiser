0ae0cac8089fd92a6475795f9794b64e14ddbdd7
vamshinath <vamshiapple.af@gmail.com>
2016-10-24 20:48:20 +0530

    almost done

--- superOrg.sh
+++ superOrg.sh
@@ -39,5 +39,16 @@ else
 	echo "SUCCESS2"
 fi
 
+mkdir -p ./$dt/images
+mkdir -p ./$dt/videos
+mkdir -p ./$dt/songs
+mkdir -p ./$dt/documents
+mkdir -p ./$dt/other
 
+find . -maxdepth 1 -type f  \( -iname '*.jpg' -or -iname '*.jpeg' -or -iname '*.png' -or -iname '*.gif'  \) -exec mv {} ./$dt/images \;
+find . -maxdepth 1 -type f  \( -iname '*.3gp' -or -iname '*.mkv' -or -iname '*.mp4' -or -iname '*.flv'  \) -exec mv {} ./$dt/videos \;
+find . -maxdepth 1 -type f  \( -iname '*.txt' -or -iname '*.odf' -or -iname '*.pdf' -or -iname '*.doc' -or -iname '*.docx' -or -iname '*.ppt' -or -iname '*.pptx' \) -exec mv {} ./$dt/documents \;
+find . -maxdepth 1 -type f -iname "*.mp3" -exec mv {} ./$dt/songs \;
+find . -maxdepth 1 -type f  -exec mv {} ./$dt/other \;
 
+exit 0
